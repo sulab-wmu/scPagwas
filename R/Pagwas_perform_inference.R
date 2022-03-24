f09aa799e7d9d5a94067949aa1b00227a7c3a6a6
DengChunyu <dengcyelena@gmail.com>
2022-03-24 10:57:03 +0800

    remove problem

--- R/Pagwas_perform_inference.R
+++ R/Pagwas_perform_inference.R
@@ -124,16 +124,6 @@ Boot_evaluate <- function(Pagwas,
     names(Pagwas$lm_results$parameters),
     Pagwas$lm_results$parameters
   )
-  if("Pathway_list" %in% names(Pagwas)){
-    Pagwas$Pathway_list<-NULL
-  }
-  if("rawPathway_list" %in% names(Pagwas)){
-    Pagwas$rawPathway_list<-NULL
-  }
-  if("VariableFeatures" %in% names(Pagwas)){
-    Pagwas$VariableFeatures<-NULL
-  }
-
   return(Pagwas)
 }
 
