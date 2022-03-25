004ca7df7f5136c28c8dc38f72da2daed84a2735
DengChunyu <dengcyelena@gmail.com>
2022-03-25 13:11:41 +0800

    Update Singlecell_heritability_contributions.R

--- R/Singlecell_heritability_contributions.R
+++ R/Singlecell_heritability_contributions.R
@@ -143,7 +143,7 @@ link_scCell_pwpca_block <- function(Pagwas, n.cores = 1) {
     pa_block$x<- bigstatsr::as_FBM( pa_block$ld_matrix_squared[] %*% x3)
     pa_block$include_in_inference <- T
 
-
+    gc()
     setTxtProgressBar(pb, which(names(Pagwas$Pathway_ld_gwas_data) == pathway) / length(Pagwas$Pathway_ld_gwas_data))
 
     return(pa_block)
