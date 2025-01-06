e25e6db3ab4b2f63bdf3dac81b388d8b8bdfba19
Dengchunyu <dengcyelena@gmail.com>
2025-01-06 15:18:23 +0800

    update error

--- R/scPagwas_main2.R
+++ R/scPagwas_main2.R
@@ -547,7 +547,7 @@ scPagwas_main2 <- function(Pagwas = NULL,
 
       message("* Get  pvalue for mp in each cell type!")
 
-      Idents(Single_data)<-Celltype_anno$merge_anno
+      Idents(Single_data)<-Pagwas$Celltype_anno$annotation
       PaHeritability_marker<-FindAllMarkers(Single_data,
           assay = "scPagwasPaHeritability",
           logfc.threshold = 0,
